13afbadb724babd4d7395f2f571c1bdf65f085c0
Vineeth-Narayan <57552723+Vineeth-Narayan@users.noreply.github.com>
2022-07-21 05:35:03 +0530

    Add files via upload

---
+++ 2_multiplier.cmd
@@ -0,0 +1,103 @@
+stepsize 10000
+h vdd
+l Gnd
+w xo x1 yo y1 p0 p1 p2 p3  
+vector in xo x1 yo y1
+setvector in 0000
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 0001
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 0010
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 0011
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 0100
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 0101
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 0110
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 0111
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 1000
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 1001
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 1010
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 1011
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 1100
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 1101
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 1110
+s
+path p0
+path p3
+path p2
+path p1
+setvector in 1111
+s
+path p0
+path p3
+path p2
+path p1
+
+
---
+++ 4_adder.cmd
@@ -0,0 +1,85 @@
+stepsize 10000
+h vdd
+l Gnd
+w b1 b2 b3 b4 a1 a1 a3 a4 so1 so2 so3 so4 co4
+vector in b1 b2 b3 b4 a1 a2 a3 a4
+setvector in 11111111
+s
+path so4
+path so3
+path so2
+path so1
+path co4
+
+setvector in 00000010
+s
+path so4
+path so3
+path so2
+path so1
+path co4
+
+setvector in 11111000
+s
+path so4
+path so3
+path so2
+path so1
+path co4
+
+setvector in 11101110
+s
+path so4
+path so3
+path so2
+path so1
+path co4
+
+setvector in 01111110
+s
+path so4
+path so3
+path so2
+path so1
+path co4
+
+setvector in 10111101
+s
+path so4
+path so3
+path so2
+path so1
+path co4
+
+setvector in 01110101
+s
+path so4
+path so3
+path so2
+path so1
+path co4
+
+setvector in 11001110
+s
+path so4
+path so3
+path so2
+path so1
+path co4
+
+setvector in 10011000
+s
+path so4
+path so3
+path so2
+path so1
+path co4
+
+setvector in 10000001
+s
+path so4
+path so3
+path so2
+path so1
+path co4
+
